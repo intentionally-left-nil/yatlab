00cd6221e491b19ba8541051540f9b85527d1f1c
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-11-09 16:19:14 -0800

    Add a question mark to all messages that have an acronym

--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
-import { RtmClient, WebClient, RTM_EVENTS } from '@slack/client';
-import { parseMessage } from './messageParser';
+import { RtmClient, WebClient, CLIENT_EVENTS, RTM_EVENTS } from '@slack/client';
+import parseMessage from './parseMessage';
+import handleReactionAdded from './handleReactionAdded';
 
 const addEvents = (rtm, web) => {
+  let botUser;
+
+  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (data) => {
+    botUser = data.self.id;
+  });
+
   rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-    console.log('message', message);
     parseMessage({web, message});
+  });
+
+  rtm.on(RTM_EVENTS.REACTION_ADDED, (message) => {
+    if (message.user === botUser) {
+      handleReactionAdded({web, rtm, message});
+      console.log('reaction by myself');
+    } else {
+      console.log('new user');
+    }
   });
 };
 
