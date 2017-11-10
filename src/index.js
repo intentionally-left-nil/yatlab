2c4e97210982f90c3abe88ac74a1b0684a55add4
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-11-10 13:48:27 -0800

    Prepare to run on heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { start } from './server';
+import express from 'express';
 
 const botToken = process.env.SLACK_BOT_TOKEN;
 const accessToken = process.env.SLACK_ACCESS_TOKEN;
 start({botToken, accessToken});
-process.stdin.resume();
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Yatlab!');
+});
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`);
+});
