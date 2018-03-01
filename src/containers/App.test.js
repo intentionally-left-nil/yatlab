712ef1189b597862b28a7374b2de3f956539362d
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-02-28 23:32:06 -0800

    Test the app inside a browserrouter

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const content = (
+    <BrowserRouter>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </BrowserRouter>
+  );
+
+  ReactDOM.render(content, document.createElement('div'));
 });
