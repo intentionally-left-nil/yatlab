56c3ecfaa2c79d5b1423b1c9db04924ef7b9d895
Anil Kulkarni <akulkarni@avvo.com>
2018-03-15 14:59:12 -0700

    Add a support email

--- src/containers/Support.js
+++ src/containers/Support.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Support = () => (
   <div>
     <h1>Support</h1>
-    <p>Should you need any assistance, please reach out to me on twitter:</p>
-    <p><a href="https://twitter.com/anilredshift">@AnilRedshift</a></p>
+    <p>Should you need any assistance, please reach out:</p>
+    <p>Twitter: <a href="https://twitter.com/anilredshift">@AnilRedshift</a></p>
+    <p>Email: <a href="mailto:support@terminal.space?Subject=Question about Yatlab">support@terminal.space</a></p>
   </div>
 );
 
