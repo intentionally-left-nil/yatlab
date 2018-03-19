88f4da79447e184ba2f63e2cc6512082bdded454
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-18 22:56:03 -0700

    Update the privacy and support page

--- src/containers/Privacy.js
+++ src/containers/Privacy.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Centered from '../components/Centered';
 
 const Privacy = () => (
-  <div>
+  <Centered>
     <h1>Privacy Policy</h1>
     <p>The privacy policy is very simple. Yatlab only stores the acronyms you enter on this website.</p>
     <p>Yatlab also stores the authorization token and team name so our bot can join your workspace. If yatlab has a problem that causes it to crash, our internal logs may inadvertantly have personal information (if a message caused a crash). We promise to strip out any identifying information before investigating.</p>
     <p>Yatlab uses a cookie so you can stay logged in, but this is not used to track you in any other way.</p>
     <p>Yatlab does <b>NOT</b> store the contents of your communications or use 3rd party tracking cookies.</p>
-  </div>
+  </Centered>
 );
 
 export default Privacy;
--- src/containers/Support.js
+++ src/containers/Support.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Centered from '../components/Centered';
 
 const Support = () => (
-  <div>
+  <Centered>
     <h1>Support</h1>
-    <p>Should you need any assistance, please reach out:</p>
+    <p>Should you need any assistance, please reach out below.</p>
     <p>Twitter: <a href="https://twitter.com/anilredshift">@AnilRedshift</a></p>
     <p>Email: <a href="mailto:support@terminal.space?Subject=Question about Yatlab">support@terminal.space</a></p>
-  </div>
+  </Centered>
 );
 
 export default Support;
