3c8beeac12156f9f282436038acd74425444bf43
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-18 23:02:11 -0700

    Style the Teams#new page

--- src/containers/Teams#new.js
+++ src/containers/Teams#new.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SlackButton from '../components/SlackButton';
+import Centered from '../components/Centered';
 
 export default (() => (
-  <div>
-    <h1>Add Yatlab to your Slack workspace</h1>
+  <Centered>
+    <h1>Install Yatlab</h1>
+    <p>Click the button below to add Yatlab to your Slack workspace. Yatlab will request permission to monitor your channels for acronyms. Please refer to our <Link to="/privacy">privacy policy</Link> for more information.</p>
     <SlackButton />
-  </div>
+  </Centered>
 ));
