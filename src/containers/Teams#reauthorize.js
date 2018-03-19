79781e363ab6a7ce7000371f71763b21c753cf73
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-18 23:03:56 -0700

    Style the Teams#reauthorize page

--- src/containers/Teams#reauthorize.js
+++ src/containers/Teams#reauthorize.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SlackButton from '../components/SlackButton';
+import Centered from '../components/Centered';
 
 export default (() => (
-  <div>
+  <Centered>
     <h1>Authorization Required</h1>
-    <div>
+    <p>
       We've updated yatlab, but in order to work properly, it needs to be reauthorized. Please click the button below to complete the process.
       Thanks!
-    </div>
+    </p>
     <SlackButton />
-  </div>
+  </Centered>
 ));
