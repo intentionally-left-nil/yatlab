f928874669af39e55691a17d72db101aa7f79803
Anil Kulkarni <akulkarni@avvo.com>
2018-04-03 15:14:29 -0700

    Use the docker env urls if DATABASE_URL isnt present

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,4 +1,12 @@
 const pgp = require('pg-promise')();
-console.debug("Connecting to ", process.env.DATABASE_URL);
-const db = pgp(process.env.DATABASE_URL);
+let url = process.env.DATABASE_URL;
+if (!url &&
+    process.env.DB_PORT_5432_TCP_ADDR &&
+    process.env.DB_PORT_5432_TCP_PORT &&
+    process.env.DB_ENV_POSTGRES_PASSWORD) {
+  url = `postgres://postgres:${process.env.DB_ENV_POSTGRES_PASSWORD}@` +
+  `${process.env.DB_PORT_5432_TCP_ADDR}:${process.env.DB_PORT_5432_TCP_PORT}/postgres`;
+}
+console.debug("Connecting to ", url);
+const db = pgp(url);
 module.exports = db;
