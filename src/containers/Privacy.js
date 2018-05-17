0e2ad5ba934a480b36f411bd755f7de9ea8d5502
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-05-16 18:34:38 -0700

    Update the privacy options

--- src/containers/Privacy.js
+++ src/containers/Privacy.js
@@ -7,7 +7,7 @@ const Privacy = () => (
   <Centered>
     <Title>Privacy Policy</Title>
     <Body>
-      <p>The privacy policy is very simple. Yatlab only stores the acronyms you enter on this website.</p>
+      <p>The privacy policy is very simple. Yatlab only stores the acronyms you enter on this website, along with usage stats for each acronym</p>
       <p>Yatlab also stores the authorization token and team name so our bot can join your workspace. If yatlab has a problem that causes it to crash, our internal logs may inadvertantly have personal information (if a message caused a crash). We promise to strip out any identifying information before investigating.</p>
       <p>Yatlab uses a cookie so you can stay logged in, but this is not used to track you in any other way.</p>
       <p>Yatlab does <b>NOT</b> store the contents of your communications or use 3rd party tracking cookies.</p>
